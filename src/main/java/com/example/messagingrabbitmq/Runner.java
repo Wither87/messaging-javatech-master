1db91b4d0655548734769f576b25e19a32057c91
Wither_87 <wither_87@mail.ru>
2021-11-25 09:50:49 +0500

    Initial commit

---
+++ src/main/java/com/example/messagingrabbitmq/Runner.java
@@ -0,0 +1,42 @@
+package com.example.messagingrabbitmq;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Runner implements CommandLineRunner {
+
+	private final RabbitTemplate rabbitTemplate;
+	private final Receiver receiver;
+
+	public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
+		this.receiver = receiver;
+		this.rabbitTemplate = rabbitTemplate;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("\n\nApplication is started!\n");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		while(true){
+			String message = reader.readLine();
+			if (message.isEmpty()){
+				continue;
+			}
+			if (message.equals("exit")){
+				break;
+			}
+
+			System.out.println("Sending message...");
+			//rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.fanoutExchangeName, "", "Hello from RabbitMQ!");
+			rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.fanoutExchangeName, "", message);
+			receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+		}
+	}
+
+}
